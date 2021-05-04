7e1a2917772a0cbba9a0146246b85fa4f0a212de
CORP\maxim.suglob <maxim.suglob@solbeg.com>
2021-05-04 14:10:24 +0300

    updated firebase credentials

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,9 +1,18 @@
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyAbe3fZjQz66_B07gRLEZs_X-K2U29b_2c",
+//   authDomain: "hr-helper-55948.firebaseapp.com",
+//   databaseURL: "https://hr-helper-55948-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "hr-helper-55948",
+//   storageBucket: "hr-helper-55948.appspot.com",
+//   messagingSenderId: "31993570192",
+//   appId: "1:31993570192:web:58f341f1d007e9da270915"
+// };
 export const firebaseConfig = {
-  apiKey: "AIzaSyAbe3fZjQz66_B07gRLEZs_X-K2U29b_2c",
-  authDomain: "hr-helper-55948.firebaseapp.com",
-  databaseURL: "https://hr-helper-55948-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "hr-helper-55948",
-  storageBucket: "hr-helper-55948.appspot.com",
-  messagingSenderId: "31993570192",
-  appId: "1:31993570192:web:58f341f1d007e9da270915"
+  apiKey: "AIzaSyAWSz7_ZArWYxtI37X4WdXJgkUX8r9xlO4",
+  authDomain: "hr-helper-5cfeb.firebaseapp.com",
+  databaseURL: "https://hr-helper-5cfeb-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hr-helper-5cfeb",
+  storageBucket: "hr-helper-5cfeb.appspot.com",
+  messagingSenderId: "772605445384",
+  appId: "1:772605445384:web:234df442dfee5abe5e98bd"
 };
